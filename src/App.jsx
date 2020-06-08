0b9dcbd7160ac14bf56aae14be92f744a82e69e7
manu <manu.villar.vitoria@gmail.com>
2020-06-08 16:36:32 +0200

    creating and reading the form and adding tasks in the State with a random ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import shortid from 'shortid';// npm i shortid (libreria de generador de ID)
 
 
 function App() {
+
+//State para guardar la tarea
+const [tarea, setTarea] = React.useState('');
+//State para guardar nuevas tareas a la existente
+const [tareas, setTareas] = React.useState([]);
+
+
+const agregarTarea = e => {
+  e.preventDefault(); 
+  if(!tarea.trim()){
+    console.log('elemento vacío')
+  }
+    console.log(tarea)
+
+    //Añadir nuevas tareas a la existente
+    setTareas([
+      ...tareas,
+      {id: shortid.generate(), nombreTarea: tarea}
+    ])
+
+   //Vacíar el formulario
+    setTarea('')
+}
+
   return (
     <div className="container mt-5">
       <h1 className="text-center">CRUD Simple con Hooks</h1>
@@ -20,6 +45,19 @@ function App() {
 
         <div className="col-4">
           <h4 className="text-center">Formulario</h4>
+          <form onSubmit={agregarTarea}>
+            <input 
+              type="text"
+              className="form-constrol mb-2"
+              placeholder="Ingresar tarea"
+              onChange={e => setTarea(e.target.value)}
+              value={tarea}
+            />
+            <button  
+              type="submit"
+              className="btn btn-dark btn-block"
+            >Agregar</button> 
+          </form>
         </div>
       </div>
     </div>
